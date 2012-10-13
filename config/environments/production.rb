ff0ed95cf2c10c02674686e0bdc8273e88730568
Andy Macdonald <andy@theandym.com>
2012-10-12 22:56:51 -0400

    fixed precompile issue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,9 @@ Railsrumble2012::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  
+  ###
+  
+  # HTML5-Rails
+  config.assets.precompile += ['polyfills.js']
 end
