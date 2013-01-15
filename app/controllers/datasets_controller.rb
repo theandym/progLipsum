c2aa2105952496f38aa994277a2976700cb4b07a
Andy Macdonald <andy@theandym.com>
2013-01-14 23:27:44 -0500

    updated address generation to utilize new mannequin functionality; added new name variable naming; updated copyright date in footer

--- app/controllers/datasets_controller.rb
+++ app/controllers/datasets_controller.rb
@@ -23,11 +23,6 @@ class DatasetsController < ApplicationController
       @number_of_entities = params[:number_of_entities].to_i
     end
     
-    @array_of_street_numbers = (1..2000).to_a
-    @array_of_street_names = ["Maple", "Oak", "Poplar", "Cherry", "Dogwood"]
-    @array_of_street_types = ["St", "Rd", "Cir", "Ct", "Way", "Ave"]
-    @array_of_cities = ["Beijing", "Bogota", "Buenos Aires", "Cairo", "Delhi", "Dhaka", "Guangzhou", "Istanbul", "Jakarta", "Karachi", "Kinshasa", "Kolkata", "Lagos", "Lima", "London", "Los Angeles", "Manila", "Mexico City", "Moscow", "Mumbai", "New York City", "Osaka", "Rio de Janeiro", "Sao Paulo", "Seoul", "Shanghai", "Shenzhen", "Tehran", "Tianjin", "Tokyo"]
-    
     @lipsum_paragraph_1 = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam id elit nisl. Sed elementum condimentum viverra. Sed at quam vitae dolor imperdiet facilisis. Maecenas lacinia, lacus ac iaculis mattis, nibh nulla iaculis ligula, a tincidunt lorem risus et erat. Donec magna nisi, mollis sit amet ullamcorper quis, pulvinar at odio. Duis ac turpis arcu, at pellentesque enim. Nulla ante quam, varius eget fringilla sit amet, rutrum eget ligula. Donec sed dui nec ligula aliquam pharetra. Suspendisse imperdiet pellentesque ipsum, et facilisis massa varius nec.'
     @lipsum_paragraph_2 = 'Cras ac dolor sit amet erat varius ullamcorper sed accumsan nisi. Sed luctus ultricies neque quis adipiscing. Vestibulum vitae auctor tortor. Suspendisse vel scelerisque urna. Nunc bibendum pulvinar eros, nec dapibus dui pulvinar eu. Curabitur erat tortor, volutpat vitae bibendum egestas, tristique eget urna. Suspendisse aliquet fermentum metus quis facilisis. Nulla facilisi. Sed commodo auctor porttitor. Etiam tempus felis vel magna consequat vel molestie tortor lobortis. Aliquam auctor tellus vel nibh feugiat in sagittis augue malesuada. Sed a ante id libero semper aliquet molestie id risus. Integer ac condimentum nibh. Vestibulum sed orci malesuada tortor mollis volutpat. Curabitur euismod euismod erat sed bibendum.'
     @lipsum_paragraph_3 = 'Nam eleifend, mauris in rhoncus pulvinar, dolor ante commodo neque, a dictum massa urna congue justo. Nullam condimentum enim a felis accumsan molestie. Aenean eu est ut leo volutpat hendrerit vitae a elit. Nam in magna est. Nulla ac consequat nisi. Praesent vel lorem est. Nullam at velit vel libero cursus tincidunt. Etiam at massa et sapien pharetra volutpat.'
@@ -44,16 +39,24 @@ class DatasetsController < ApplicationController
     
     @number_of_entities.times do
       @person = Mannequin::Person.new
+      @address = Mannequin::Address.new
       
       map_variables_to_data = {
-        "[[name_first]]" => @person.first_name,
-        "[[name_last]]" => @person.last_name,
+        "[[name_first]]" => @person.first_name, #deprecated
+        "[[first_name]]" => @person.first_name,
+        "[[name_last]]" => @person.last_name, #deprecated
+        "[[last_name]]" => @person.last_name,
         "[[initial]]" => @person.middle_initial,
+        "[[middle_initial]]" => @person.middle_initial,
         "[[email_address]]" => @person.email_address,
-        "[[street_number]]" => @array_of_street_numbers.sample,
-        "[[street_name]]" => @array_of_street_names.sample,
-        "[[street_type]]" => @array_of_street_types.sample,
-        "[[city]]" => @array_of_cities.sample,
+        "[[street_address]]" => @address.street_address,
+        "[[street_number]]" => @address.street_number,
+        "[[street_name]]" => @address.street_name,
+        "[[street_type]]" => @address.street_type,
+        "[[city]]" => @address.city,
+        "[[state]]" => @address.state,
+        "[[state_code]]" => @address.state_code,
+        "[[country]]" => @address.country,
         "[[number_2_digit]]" => 10 + Random.rand(99 - 10),
         "[[number_3_digit]]" => 100 + Random.rand(999 - 100),
         "[[number_4_digit]]" => 1000 + Random.rand(9999 - 1000),
