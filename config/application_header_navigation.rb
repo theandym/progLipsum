a5e48e55f33de273839dfd2c8e216a7b011dfd4b
Andy Macdonald <andy@theandym.com>
2013-01-21 22:49:04 -0500

    overhauled color scheme; updated minor layout issues; updated some devise erb templates to haml

--- config/application_header_navigation.rb
+++ config/application_header_navigation.rb
@@ -3,12 +3,12 @@ SimpleNavigation::Configuration.run do |navigation|
 
   navigation.items do |primary|
     if account_signed_in?
-      primary.item :datasets, 'Datasets', datasets_path
-      primary.item :account, 'Account', edit_account_registration_path
-      primary.item :logout, 'Log Out', destroy_account_session_path, :method => :delete
+      primary.item :datasets, 'Datasets', datasets_path, :link => {:class => 'button blue small'}
+      primary.item :account, 'Account', edit_account_registration_path, :link => {:class => 'button blue small'}
+      primary.item :logout, 'Log Out', destroy_account_session_path, :link => {:class => 'button small'}, :method => :delete
     else
-      primary.item :signup, 'Sign Up', new_account_registration_path
-      primary.item :login, 'Log In', new_account_session_path
+      primary.item :signup, 'Sign Up', new_account_registration_path, :link => {:class => 'button blue small'}
+      primary.item :login, 'Log In', new_account_session_path, :link => {:class => 'button small'}
     end
   end
 
